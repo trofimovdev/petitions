da29ef236ac74dbcdd9e69ddd92874e33ade9668
Anatoly <20482479+trofimovdev@users.noreply.github.com>
2020-05-31 19:19:06 +0300

    fix splashscreen

--- backend/app/Models/Petition.php
+++ backend/app/Models/Petition.php
@@ -199,7 +199,28 @@ class Petition extends Model
 
     public static function filterString(string $string)
     {
-        return preg_replace("/[^[:print:]\s]/", "", $string);
+        // Match Emoticons
+        $regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
+        $clear_string = preg_replace($regex_emoticons, '', $string);
+
+        // Match Miscellaneous Symbols and Pictographs
+        $regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
+        $clear_string = preg_replace($regex_symbols, '', $clear_string);
+
+        // Match Transport And Map Symbols
+        $regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
+        $clear_string = preg_replace($regex_transport, '', $clear_string);
+
+        // Match Miscellaneous Symbols
+        $regex_misc = '/[\x{2600}-\x{26FF}]/u';
+        $clear_string = preg_replace($regex_misc, '', $clear_string);
+
+        // Match Dingbats
+        $regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
+        $clear_string = preg_replace($regex_dingbats, '', $clear_string);
+
+        $clear_string = preg_replace("/[^\p{L}\p{N}\p{P}\s]/u", "", $clear_string);
+        return htmlspecialchars_decode(htmlspecialchars($clear_string, ENT_SUBSTITUTE, 'UTF-8'));
     }
 
     public static function exifRotate($image)
--- frontend/src/components/EditPetition/EditPetition.js
+++ frontend/src/components/EditPetition/EditPetition.js
@@ -117,7 +117,7 @@ const EditPetition = ({
       ...form,
       ...{
         [name]: value
-          .replace(/[^[:print:]\s]/, "")
+          .replace(/[^[:print:]\s]/g, "")
           .replace(
             /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
             ""
