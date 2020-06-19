5322c359ba586343df7b84dca495c40ddeb31cd4
Anatoly <20482479+trofimovdev@users.noreply.github.com>
2020-06-19 03:23:35 +0300

    remove swipeable tabs

--- frontend/src/components/PetitionsFeed/PetitionsFeed.js
+++ frontend/src/components/PetitionsFeed/PetitionsFeed.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import {
   HorizontalScroll,
   Panel,
@@ -18,8 +18,6 @@ import "./PetititonsFeed.css";
 import { VKMiniAppAPI } from "@vkontakte/vk-mini-apps-api";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import SwipeableViews from "react-swipeable-views";
-import smoothscroll from "smoothscroll-polyfill";
 import ErrorCard from "../ErrorCard/ErrorCard";
 import PetitionCard from "../PetitionCard/PetitionCard";
 import EpicTabbar from "../EpicTabbar/EpicTabbar";
@@ -29,7 +27,6 @@ import { setPopular, setLast, setSigned } from "../../store/petitions/actions";
 import { loadPetitions } from "../../tools/helpers";
 
 const api = new VKMiniAppAPI();
-smoothscroll.polyfill();
 
 const PetitionsFeed = ({
   id,
@@ -37,58 +34,17 @@ const PetitionsFeed = ({
   activeTab,
   activeStory,
   setStory,
+  currentPetitions,
   activePanel,
   launchParameters,
   setPopular,
   setLast,
   setSigned,
-  initError,
-  popularPetitions,
-  lastPetitions,
-  signedPetitions
+  initError
 }) => {
   const [fetchingStatus, setFetchingStatus] = useState(false);
   const [loadingStatus, setLoadingStatus] = useState(false);
   const [endStatus, setEndStatus] = useState(false);
-  const [indexTab, setIndexTab] = useState(
-    activeTab.feed === "popular"
-      ? 0
-      : activeTab.feed === "last"
-      ? 1
-      : activeTab.feed === "signed"
-      ? 2
-      : 1
-  );
-
-  const handleChangeIndex = useCallback(
-    event => {
-      const HS = document.getElementsByClassName("HorizontalScroll__in")[0];
-      switch (event) {
-        case 0:
-        case "popular":
-          setIndexTab(0);
-          setActiveTab("feed", "popular");
-          HS.scroll({ top: 0, left: 0, behavior: "smooth" });
-          break;
-
-        default:
-        case 1:
-        case "last":
-          setIndexTab(1);
-          setActiveTab("feed", "last");
-          HS.scroll({ top: 0, left: 4, behavior: "smooth" });
-          break;
-
-        case 2:
-        case "signed":
-          setIndexTab(2);
-          setActiveTab("feed", "signed");
-          HS.scroll({ top: 0, left: 8, behavior: "smooth" });
-          break;
-      }
-    },
-    [setActiveTab]
-  );
 
   const setCurrentPetitions = petitions => {
     switch (activeTab.feed) {
@@ -140,22 +96,6 @@ const PetitionsFeed = ({
       return;
     }
     const petitionsContainerHeight = petitionsContainer.offsetHeight;
-
-    let currentPetitions = [];
-    switch (activeTab.feed) {
-      case "popular":
-        currentPetitions = popularPetitions;
-        break;
-
-      default:
-      case "last":
-        currentPetitions = lastPetitions;
-        break;
-
-      case "signed":
-        currentPetitions = signedPetitions;
-        break;
-    }
 
     if (
       currentPetitions &&
@@ -221,7 +161,7 @@ const PetitionsFeed = ({
       api.setLocationHash(activeTab.feed);
     }
     return () => {};
-  }, [activePanel, activeTab.feed]);
+  }, [activeTab, activePanel]);
 
   const platform = usePlatform();
 
@@ -241,19 +181,19 @@ const PetitionsFeed = ({
               <Tabs>
                 <HorizontalScroll>
                   <TabsItem
-                    onClick={() => handleChangeIndex(0)}
+                    onClick={() => setActiveTab("feed", "popular")}
                     selected={activeTab.feed === "popular"}
                   >
                     Популярные
                   </TabsItem>
                   <TabsItem
-                    onClick={() => handleChangeIndex(1)}
+                    onClick={() => setActiveTab("feed", "last")}
                     selected={activeTab.feed === "last"}
                   >
                     Последние
                   </TabsItem>
                   <TabsItem
-                    onClick={() => handleChangeIndex(2)}
+                    onClick={() => setActiveTab("feed", "signed")}
                     selected={activeTab.feed === "signed"}
                   >
                     Подписанные
@@ -264,119 +204,52 @@ const PetitionsFeed = ({
           )}
         </div>
       </PanelHeaderSimple>
-      <PullToRefresh
-        onRefresh={onRefresh}
-        isFetching={fetchingStatus}
-        id="petitionsContainer"
-      >
-        {!launchParameters.vk_group_id ? (
-          <SwipeableViews index={indexTab} onChangeIndex={handleChangeIndex}>
-            {[popularPetitions, lastPetitions, signedPetitions].map(
-              (currentPetitions, i) => {
-                if (initError) {
-                  return (
-                    <div key={i}>
-                      <ErrorCard />
-                    </div>
-                  );
-                }
-                if (!currentPetitions) {
-                  return (
-                    <div key={i}>
-                      <Spinner
-                        size="regular"
-                        className="PetitionsFeed__spinner"
-                      />
-                    </div>
-                  );
-                }
-                return (
-                  <div key={i}>
-                    <FriendsCard />
-                    {currentPetitions.map((item, index) => {
-                      return (
-                        <div key={index}>
-                          <PetitionCard
-                            id={item.id}
-                            title={item.title}
-                            countSignatures={item.count_signatures}
-                            needSignatures={item.need_signatures}
-                            mobilePhotoUrl={item.mobile_photo_url}
-                            friends={item.friends || []}
-                            completed={item.completed}
-                          />
-                          {index < currentPetitions.length - 1 && <Separator />}
-                        </div>
-                      );
-                    })}
-                    {currentPetitions.length === 0 &&
-                    activeTab.feed === "popular" ? (
-                      <Footer>Скоро здесь будут популярные петиции</Footer>
-                    ) : currentPetitions.length === 0 &&
-                      activeTab.feed === "signed" ? (
-                      <Footer>Пока что Вы не подписали ни одной петиции</Footer>
-                    ) : currentPetitions.length === 0 &&
-                      activeTab.feed === "last" ? (
-                      <Footer>Пока что нет ни одной петиции</Footer>
-                    ) : (currentPetitions.length > 0 && endStatus) ||
-                      (currentPetitions.length > 0 &&
-                        currentPetitions.length < 10 &&
-                        !endStatus) ? (
-                      <></>
-                    ) : (
-                      <Spinner
-                        size="regular"
-                        className="PetitionsFeed__spinner__bottom"
-                      />
-                    )}
-                  </div>
-                );
-              }
-            )}
-          </SwipeableViews>
-        ) : (
-          <>
-            {initError ? (
-              <ErrorCard />
-            ) : !lastPetitions ? (
-              <Spinner size="regular" className="PetitionsFeed__spinner" />
-            ) : (
-              <>
-                <FriendsCard />
-                {lastPetitions.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <PetitionCard
-                        id={item.id}
-                        title={item.title}
-                        countSignatures={item.count_signatures}
-                        needSignatures={item.need_signatures}
-                        mobilePhotoUrl={item.mobile_photo_url}
-                        friends={item.friends || []}
-                        completed={item.completed}
-                      />
-                      {index < lastPetitions.length - 1 && <Separator />}
-                    </div>
-                  );
-                })}
-                {lastPetitions.length === 0 ? (
-                  <Footer>Пока что нет ни одной петиции</Footer>
-                ) : (lastPetitions.length > 0 && endStatus) ||
-                  (lastPetitions.length > 0 &&
-                    lastPetitions.length < 10 &&
-                    !endStatus) ? (
-                  <></>
-                ) : (
-                  <Spinner
-                    size="regular"
-                    className="PetitionsFeed__spinner__bottom"
-                  />
-                )}
-              </>
-            )}
-          </>
-        )}
-      </PullToRefresh>
+      {initError ? (
+        <ErrorCard />
+      ) : currentPetitions !== undefined ? (
+        <PullToRefresh
+          onRefresh={onRefresh}
+          isFetching={fetchingStatus}
+          id="petitionsContainer"
+        >
+          <FriendsCard />
+          {currentPetitions.map((item, index) => {
+            return (
+              <div key={index}>
+                <PetitionCard
+                  id={item.id}
+                  title={item.title}
+                  countSignatures={item.count_signatures}
+                  needSignatures={item.need_signatures}
+                  mobilePhotoUrl={item.mobile_photo_url}
+                  friends={item.friends || []}
+                  completed={item.completed}
+                />
+                {index < currentPetitions.length - 1 && <Separator />}
+              </div>
+            );
+          })}
+          {currentPetitions.length === 0 && activeTab.feed === "popular" ? (
+            <Footer>Скоро здесь будут популярные петиции</Footer>
+          ) : currentPetitions.length === 0 && activeTab.feed === "signed" ? (
+            <Footer>Пока что Вы не подписали ни одной петиции</Footer>
+          ) : currentPetitions.length === 0 && activeTab.feed === "last" ? (
+            <Footer>Пока что нет ни одной петиции</Footer>
+          ) : (currentPetitions.length > 0 && endStatus) ||
+            (currentPetitions.length > 0 &&
+              currentPetitions.length < 10 &&
+              !endStatus) ? (
+            <></>
+          ) : (
+            <Spinner
+              size="regular"
+              className="PetitionsFeed__spinner__bottom"
+            />
+          )}
+        </PullToRefresh>
+      ) : (
+        <Spinner size="regular" className="PetitionsFeed__spinner" />
+      )}
 
       <EpicTabbar activeStory={activeStory} setStory={setStory} />
     </Panel>
@@ -387,11 +260,9 @@ const mapStateToProps = state => {
   return {
     activeStory: state.router.activeStory,
     activePanel: state.router.activePanel,
+    currentPetitions: state.petitions[state.router.activeTab.feed],
     launchParameters: state.data.launchParameters,
-    initError: state.data.initError,
-    popularPetitions: state.petitions.popular,
-    lastPetitions: state.petitions.last,
-    signedPetitions: state.petitions.signed
+    initError: state.data.initError
   };
 };
 
@@ -418,15 +289,13 @@ PetitionsFeed.propTypes = {
   activeTab: PropTypes.object.isRequired,
   activeStory: PropTypes.string.isRequired,
   setStory: PropTypes.func.isRequired,
+  currentPetitions: PropTypes.array,
   activePanel: PropTypes.string.isRequired,
   launchParameters: PropTypes.object.isRequired,
   setPopular: PropTypes.func.isRequired,
   setLast: PropTypes.func.isRequired,
   setSigned: PropTypes.func.isRequired,
-  initError: PropTypes.bool,
-  popularPetitions: PropTypes.array,
-  lastPetitions: PropTypes.array,
-  signedPetitions: PropTypes.array
+  initError: PropTypes.bool
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PetitionsFeed);
